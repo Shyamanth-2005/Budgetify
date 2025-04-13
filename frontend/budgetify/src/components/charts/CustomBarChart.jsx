a8f70772006b23cffa5c43ba84002aa6ffc4e367
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2025-04-13 21:42:48 +0530

    Fixed the bar color alteration

--- frontend/budgetify/src/components/charts/CustomBarChart.jsx
+++ frontend/budgetify/src/components/charts/CustomBarChart.jsx
@@ -21,7 +21,7 @@ const CustomBarChart = ({ data }) => {
       return (
         <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
           <p className="text-xs font-semi-bold text-purple-800 mb-1">
-            {payload[0].payload.source}
+            {payload[0].payload.category}
           </p>
           <p className="text-sm text-gray-600">
             Amount:
